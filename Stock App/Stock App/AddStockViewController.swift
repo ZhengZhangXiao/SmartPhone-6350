41707a65949a31e68a41f6b3de39718b26ca83db
ZhengZhangXiao <zheng.yang4@northeastern.edu>
2023-10-26 12:16:49 -0700

    Quiz

---
+++ Stock App/Stock App/AddStockViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddStockViewController.swift
+//  Stock App
+//
+//  Created by Evie Zheng on 10/25/23.
+//
+
+import UIKit
+
+class AddStockViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Stock App/Stock App/Utils/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  Stock App
+//
+//  Created by Evie Zheng on 10/26/23.
+//
+
+import Foundation
