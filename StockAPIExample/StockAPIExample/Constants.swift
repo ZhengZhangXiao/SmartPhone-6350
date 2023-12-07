05b5e038171ae81ee9e58f8ebc619bb0dc8338c5
ZhengZhangXiao <zheng.yang4@northeastern.edu>
2023-12-06 17:11:24 -0800

    push final exam

---
+++ StockAPIExample/StockAPIExample/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  StockAPIExample
+//
+//  Created by Evie Zheng on 12/6/23.
+//
+
+import Foundation
+
+let baseURL = "https://financialmodelingprep.com/api/v3/quote-short/"
+let apiKey = "713148f2eca9d4d12eadbf78dfd7bf50"
---
+++ StockAPIExample/StockAPIExample/StockClass.swift
@@ -0,0 +1,14 @@
+//
+//  StockClass.swift
+//  StockAPIExample
+//
+//  Created by Evie Zheng on 12/6/23.
+//
+
+import Foundation
+
+class StockClass {
+    var symbol: String = ""
+    var price: Float = 0.0
+    var volume: Int = 0
+}
