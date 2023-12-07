182cccf6f249984324a30980de412199d563d282
ZhengZhangXiao <zheng.yang4@northeastern.edu>
2023-12-06 16:18:14 -0800

    Push final 4

---
+++ NamePhoneNumberProtocolDelegate/NamePhoneNumberProtocolDelegate/SendNamePhoneNumberDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  SendNamePhoneNumberDelegate.swift
+//  NamePhoneNumberProtocolDelegate
+//
+//  Created by Evie Zheng on 12/6/23.
+//
+
+import Foundation
+
+protocol SendNamePhoneNumberDelegate{
+    func sendNamePhoneNumber(name: String, phoneNumber: String)
+}
