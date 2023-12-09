3d26c36037e7a3b1f2f5f81709800dc0440c6804
ZhengZhangXiao <zheng.yang4@northeastern.edu>
2023-12-09 15:21:38 -0800

    final exam 1

---
+++ WeatherApP/WeatherApP/StockClass.swift
@@ -0,0 +1,15 @@
+//
+//  StockClass.swift
+//  WeatherApP
+//
+//  Created by Evie Zheng on 12/9/23.
+//
+
+import Foundation
+
+class StockClass {
+    var cityCode: String = ""
+    var city: String = ""
+    var temperature: Int = 0
+    var conditions: String = ""
+}
